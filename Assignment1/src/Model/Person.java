806cbeb4be136d6d3a43d122387888ca35c30a6d
pradhanprac <pradhan.prac@northeastern.edu>
2024-09-19 19:57:02 -0400

    first assigmtment commit

---
+++ Assignment1/src/Model/Person.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author prach
+ */
+public class Person {
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getQualification() {
+        return Qualification;
+    }
+
+    public void setQualification(String Qualification) {
+        this.Qualification = Qualification;
+    }
+
+    public String getProfession() {
+        return Profession;
+    }
+
+    public void setProfession(String Profession) {
+        this.Profession = Profession;
+    }
+
+    public String getOrganization() {
+        return Organization;
+    }
+
+    public void setOrganization(String Organization) {
+        this.Organization = Organization;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDriversLicenseno() {
+        return DriversLicenseno;
+    }
+
+    public void setDriversLicenseno(String DriversLicenseno) {
+        this.DriversLicenseno = DriversLicenseno;
+    }
+
+    public String getDriversLicensestate() {
+        return DriversLicensestate;
+    }
+
+    public void setDriversLicensestate(String DriversLicensestate) {
+        this.DriversLicensestate = DriversLicensestate;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getAddr1() {
+        return addr1;
+    }
+
+    public void setAddr1(String addr1) {
+        this.addr1 = addr1;
+    }
+
+    public String getAadr2() {
+        return aadr2;
+    }
+
+    public void setAadr2(String aadr2) {
+        this.aadr2 = aadr2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFax() {
+        return Fax;
+    }
+
+    public void setFax(String Fax) {
+        this.Fax = Fax;
+    }
+
+    String firstname;
+    String lastname;
+    String Gender;
+    String DOB;
+    String Age;
+    String Qualification;
+    String Profession;
+    String Organization;
+    String email;
+    String Phone;
+    String DriversLicenseno;
+    String DriversLicensestate;
+    String SSN;
+    String addr1;
+    String aadr2;
+    String City;
+    String state;
+    String ZIP;
+    String telephone;
+    String Fax;
+    
+    
+
+}
