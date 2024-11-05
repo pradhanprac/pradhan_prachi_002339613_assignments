0c289127c8f4a6d52bb1b76ef16c5d1aa9f5d8d9
pradhanprac <pradhan.prac@northeastern.edu>
2024-11-04 23:07:06 -0500

    commit 6

--- Info5100-University-Example/Info5100-University-Example/src/info5100/university/example/Info5001UniversityExample.java
+++ Info5100-University-Example/Info5100-University-Example/src/info5100/university/example/Info5001UniversityExample.java
@@ -44,75 +44,7 @@ public class Info5001UniversityExample {
     public static void main(String[] args) {
         // TODO code application logic here
         
-//        Department department = new Department("Information Systems");
-//        // we need the persondirectory to create persons for the different roles
-//        
-//        PersonDirectory persondirectory = department.getPersonDirectory();
-//        
-////      Create course and keep it in the course catalog
-//        
-//        CourseCatalog coursecatalog = department.getCourseCatalog();        
-//        Course course = coursecatalog.newCourse("algorithms", "info 6205", 4);
-//        Course course2 = coursecatalog.newCourse("App Engineering", "info 5100", 4);
-//
-////      now lets schedule class to run for spring 2024        
-//        CourseSchedule courseschedule = department.newCourseSchedule("Spring 2024");
-//
-//        CourseOffer courseoffer = courseschedule.newCourseOffer("info 6205");
-//        CourseOffer courseoffer2 = courseschedule.newCourseOffer("info 5100");
-//
-//        if (courseoffer==null)return; 
-//        
-//        courseoffer.generatSeats(30); // allow for 30 open seats in the class
-//        courseoffer2.generatSeats(50); // allow for 30 open seats in the class
-//
-////      Create teacher for class
-//
-//        Person person1 = persondirectory.newPerson("01123022"); //person for teacher role                  
-//        Person person2 = persondirectory.newPerson("011230222"); 
-//        FacultyDirectory facultydirectory = department.getFacultyDirectory();
-//        FacultyProfile teacher = facultydirectory.newFacultyProfile(person1);
-//        FacultyProfile teacher2 = facultydirectory.newFacultyProfile(person2);       
-//       
-//        courseoffer.AssignAsTeacher(teacher);//  teacher.AssignAsTeacher(courseoffer); // OR  both work
-//        courseoffer2.AssignAsTeacher(teacher2);//  teacher.AssignAsTeacher(courseoffer); // OR  both work
-//       
-//    //  Create student     
-// 
-//        Person person3 = persondirectory.newPerson("0112303");  
-//        Person person4 = persondirectory.newPerson("01123034");   
-//        
-//        StudentDirectory sd = department.getStudentDirectory();
-//        StudentProfile student = sd.newStudentProfile(person3); 
-//        StudentProfile student2 = sd.newStudentProfile(person4);
-//        
-//    //  Register student in class for the load of the spring semester     
-//        CourseLoad courseload = student.newCourseLoad("Spring 2024"); 
-//        CourseLoad courseload2 = student2.newCourseLoad("Spring 2024");        
-//        courseload.registerStudentInClass(courseoffer); //register student in class Info 6205
-//        courseload.registerStudentInClass(courseoffer2);//same student signed up for 2 classes
-//        courseload2.registerStudentInClass(courseoffer2);
-//        
-//        int total = department.calculateRevenuesBySemester("Spring 2024");
-//        System.out.print("Total:  " + total+"    ");
-//        
-//       for (SeatAssignment sa : courseload.getSeatAssignments()) {
-//    if (sa.getSeat().getCourseOffer() == courseoffer) {
-//        sa.setGrade(3.7f); // Assign grade for "algorithms"
-//    }
-//    if (sa.getSeat().getCourseOffer() == courseoffer2) {
-//        sa.setGrade(3.5f); // Assign grade for "App Engineering"
-//    }
-//}
-//
-//// Find existing seat assignments for student2 and set the grades
-//for (SeatAssignment sa : courseload2.getSeatAssignments()) {
-//    if (sa.getSeat().getCourseOffer() == courseoffer2) {
-//        sa.setGrade(3.9f); // Assign grade for "App Engineering"
-//    }
-//}
-//department.printStudentReport("Spring 2024");
-//System.out.println("bbb");
+
     Department department = new Department("Information Systems");
 
     // Create a PersonDirectory to hold people
@@ -127,14 +59,7 @@ public class Info5001UniversityExample {
     Course elective4 = courseCatalog.newCourse("Database Systems", "info 6200", 3);
     Course elective5 = courseCatalog.newCourse("Cybersecurity", "info 5200", 3);
 
-    // Add courses to the degree program (core + electives)
-//    department.addCoreCourse(coreCourse);
-//    department.addElectiveCourse(elective1);
-//    department.addElectiveCourse(elective2);
-//    department.addElectiveCourse(elective3);
-//    department.addElectiveCourse(elective4);
-//    department.addElectiveCourse(elective5);
-
+    
     // Create a CourseSchedule for Spring 2024
     CourseSchedule courseSchedule = department.newCourseSchedule("Spring 2024");
 
@@ -216,23 +141,6 @@ CourseLoad courseload8 = studentp8.newCourseLoad("Spring 2024");
 CourseLoad courseload9 = studentp9.newCourseLoad("Spring 2024");
 CourseLoad courseload10 = studentp10.newCourseLoad("Spring 2024");
 
-
-
-
-//    for (int i = 1; i <= 10; i++) {
-//        Person studentPerson = personDirectory.newPerson("stu" + i);
-//        StudentProfile student = studentDirectory.newStudentProfile(studentPerson);
-//        
-        // Register each student for at least 3 classes
-//        CourseLoad courseLoad = student.newCourseLoad("Spring 2024");
-//        courseLoad.registerStudentInClass(coreOffer);
-//        courseLoad.registerStudentInClass(electiveOffer1);
-//        courseLoad.registerStudentInClass(electiveOffer2);
-//        courseLoad.registerStudentInClass(electiveOffer3);
-//        courseLoad.registerStudentInClass(electiveOffer4);
-//        courseLoad.registerStudentInClass(electiveOffer5);
-//    }
-
  courseload1.registerStudentInClass(coreOffer); // register student in core class
     courseload2.registerStudentInClass(electiveOffer1); // student 2 in Algorithms
     courseload3.registerStudentInClass(electiveOffer2); // student 3 in App Engineering
@@ -242,7 +150,8 @@ CourseLoad courseload10 = studentp10.newCourseLoad("Spring 2024");
     courseload6.registerStudentInClass(electiveOffer5); // student 6 in Cybersecurity
     courseload7.registerStudentInClass(coreOffer); // student 7 in core class
     courseload8.registerStudentInClass(electiveOffer1); // student 8 in Algorithms
-    courseload9.registerStudentInClass(electiveOffer2); // student 9 in App Engineering
+    courseload9.registerStudentInClass(electiveOffer2);
+    courseload9.registerStudentInClass(electiveOffer5); // student 9 in App Engineering
     courseload10.registerStudentInClass(electiveOffer3); // student 10 in Web Design
     courseload1.registerStudentInClass(electiveOffer4); // student 1 in Database Systems
     courseload2.registerStudentInClass(electiveOffer5); // student 2 in Cybersecurity
@@ -250,6 +159,7 @@ CourseLoad courseload10 = studentp10.newCourseLoad("Spring 2024");
     courseload4.registerStudentInClass(electiveOffer1); // student 4 in Algorithms
     courseload5.registerStudentInClass(electiveOffer2); // student 5 in App Engineering
     courseload6.registerStudentInClass(electiveOffer3); // student 6 in Web Design
+    courseload6.registerStudentInClass(electiveOffer4);
     courseload7.registerStudentInClass(electiveOffer4); // student 7 in Database Systems
     courseload8.registerStudentInClass(electiveOffer5); // student 8 in Cybersecurity
     courseload8.registerStudentInClass(electiveOffer4);
